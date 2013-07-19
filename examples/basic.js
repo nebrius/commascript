adbd234dd4641933d0ecdaf19fa04b777c08fd12
Bryan Hughes <bryan@Bryan-Hughess-MacBook-Pro-15.local>
2013-07-18 23:44:12 -0700

    Stubbed out all of the AST rules and fleshed out how rules are validated

--- examples/basic.js
+++ examples/basic.js
@@ -1,6 +1,8 @@
-'use commatype';
+'use commascript';
 
 // Primitive data types are a simple tuple
 var num = ('number', 10),
 	bool = ('boolean', true),
 	str = ('string', 'hello world');
+
+num = bool;
