f586644bd6c959ad4ffc1b3a0791309ee61bf573
Bryan Hughes <bryan@Bryan-Hughess-MacBook-Pro-15.local>
2013-07-23 21:54:16 -0700

    Added object definition syntax support

--- examples/basic.js
+++ examples/basic.js
@@ -1,8 +1,21 @@
 'use commascript';
 
+('#object', {
+	name: 'foo',
+	properties: {
+		'bar': 'string',
+		'baz': 'number'
+	},
+	prototype: 'object',
+	constructorArgumentTypes: ['string', 'number']
+});
+
 // Primitive data types are a simple tuple
 var num = ('number', 10),
 	bool = ('boolean', true),
-	str = ('string', 'hello world');
+	obj = ('foo', {
+		'bar': 'hello world',
+		'baz': 10
+	});
 
 num = bool;
